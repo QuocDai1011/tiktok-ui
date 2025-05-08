308205b33953df6fd6f145e8ebcf95de6e069678
QuocDai1011 <146009504+QuocDai1011@users.noreply.github.com>
2025-05-08 23:59:30 +0700

    create folders and files

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
+import { Fragment } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { publicRoutes } from '~/routes';
+import { DefaultLayout } from '~/component/Layout';
+
 function App() {
     return (
-        <div className="App">
-            <h1>Hello anh em</h1>
-        </div>
+        <Router>
+            <div className="App">
+                <Routes>
+                    {publicRoutes.map((route, index) => {
+                        var Layout = DefaultLayout;
+
+                        if (route.layout) Layout = route.layout;
+                        else if (route.layout === null) Layout = Fragment;
+                        
+                        return (
+                            <Route
+                                path={route.path}
+                                element={
+                                    <Layout>
+                                        <route.component />
+                                    </Layout>
+                                }
+                                key={index}
+                            />
+                        );
+                    })}
+                </Routes>
+            </div>
+        </Router>
     );
 }
 
