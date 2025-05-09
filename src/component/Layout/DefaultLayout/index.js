06bf8e22b8bf706672cb517fb48de353604526a9
QuocDai1011 <146009504+QuocDai1011@users.noreply.github.com>
2025-05-09 23:06:06 +0700

    add classNames library and edit .scss files

--- src/component/Layout/DefaultLayout/index.js
+++ src/component/Layout/DefaultLayout/index.js
@@ -1,17 +1,20 @@
 import Header from '../component/Header';
 import Sidebar from './Sidebar';
+import styles from './DefaultLayout.module.scss';
+import classNames from 'classnames/bind';
+
+const cx = classNames.bind(styles);
 
 function DefaultLayout({ children }) {
     return (
-        <div>
+        <div className={cx('wrapper')}>
             <Header />
-            <div className="container">
+            <div className={cx('container')}>
                 <Sidebar />
-                <div className="content">{children}</div>
+                <div className={cx('content')}>{children}</div>
             </div>
         </div>
     );
 }
 
 export default DefaultLayout;
-
